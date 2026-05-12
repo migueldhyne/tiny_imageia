51a6f5633d89aaa35732b67288e1ee0c78300ed0
migueldhyne <87825608+migueldhyne@users.noreply.github.com>
2026-05-12 20:25:34 +0200

    Add files via upload

---
+++ lang/en/tiny_imageia.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for tiny_imageia plugin.
+ *
+ * @package    tiny_imageia
+ * @copyright  2026 Miguël Dhyne <miguel.dhyne@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['apikey']            = 'Cle API OpenAI';
+$string['apikey_desc']       = 'Entrez votre cle API OpenAI (sk-...). Obtenez-la sur platform.openai.com.';
+$string['buttontext']        = 'Image IA';
+$string['buttontitle']       = 'Generer une image IA';
+$string['defaultmodel']      = 'Modele par defaut';
+$string['defaultmodel_desc'] = 'Modele OpenAI utilise par defaut. gpt-image-2 est recommande pour les usages pedagogiques.';
+$string['imagesize']         = 'Taille des images';
+$string['imagesize_desc']    = 'Taille par defaut des images generees.';
+$string['menutext']          = 'Generer une image IA pedagogique';
+$string['model']             = 'Modele de generation';
+$string['model_desc']        = 'Choisissez le modele OpenAI pour la generation d\'images.';
+$string['pluginname']        = 'Image IA pedagogique';
+$string['privacy:metadata']  = 'Le plugin tiny_imageia envoie les prompts a l\'API OpenAI pour generer des images. Aucune donnee personnelle n\'est stockee localement.';
