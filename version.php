4998e5af36fb631533ad2e838f306002ad943a20
migueldhyne <87825608+migueldhyne@users.noreply.github.com>
2026-05-12 23:25:22 +0200

    Add files via upload

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026051218;
+$plugin->version   = 2026051219;
 $plugin->requires  = 2023100900;
 $plugin->component = 'tiny_imageia';
 $plugin->maturity  = MATURITY_STABLE;
